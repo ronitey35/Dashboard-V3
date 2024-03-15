e959e1e39599c0d9704bdb1bb76816f055986de7
flashey35 <aswinbhusal35@gmail.com>
2024-03-15 14:40:13 +0545

    Users

--- src/pages/users/Users.tsx
+++ src/pages/users/Users.tsx
@@ -1,7 +1,69 @@
+import { GridColDef, GridValueGetterParams } from "@mui/x-data-grid";
+import DataTable from "../../components/dataTable/DataTable";
 import "./users.scss";
+import { userRows } from "../../data";
 
 const Users = () => {
-  return <div className="users">Users</div>;
+  const columns: GridColDef[] = [
+    { field: "id", headerName: "ID", width: 90 },
+    {
+      field: "avatar",
+      headerName: "Avatar",
+      width: 100,
+      renderCell: (params) => {
+        return <img src={params.row.img || "/noavatar.png"} alt="No Avatar " />;
+      },
+    },
+    {
+      field: "firstName",
+      headerName: "First name",
+      width: 100,
+      editable: true,
+    },
+    {
+      field: "lastName",
+      headerName: "Last name",
+      width: 120,
+      editable: true,
+    },
+    {
+      field: "email",
+      headerName: " Email",
+      width: 180,
+      editable: true,
+    },
+    {
+      field: "phone",
+      headerName: " Phone",
+      width: 150,
+      editable: true,
+    },
+    {
+      field: "age",
+      headerName: "Age",
+      type: "number",
+      width: 50,
+      editable: true,
+    },
+    {
+      field: "fullName",
+      headerName: "Full name",
+      description: "This column has a value getter and is not sortable.",
+      sortable: false,
+      width: 160,
+      valueGetter: (params: GridValueGetterParams) =>
+        `${params.row.firstName || ""} ${params.row.lastName || ""}`,
+    },
+  ];
+  return (
+    <div className="users">
+      <div className="info">
+        <h1>Users</h1>
+        <button>Add new Users</button>
+      </div>
+      <DataTable slug="users" columns={columns} rows={userRows} />
+    </div>
+  );
 };
 
 export default Users;
