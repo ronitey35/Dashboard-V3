ad6eca7c724dd64dc686029b7e796812d9cb8204
flashey35 <aswinbhusal35@gmail.com>
2024-03-15 16:53:51 +0545

    Products

---
+++ src/components/add/Add.tsx
@@ -0,0 +1,45 @@
+import { GridColDef } from "@mui/x-data-grid";
+import "./add.scss";
+
+type Props = {
+  slug: string;
+  columns: GridColDef[];
+  setOpen: React.Dispatch<React.SetStateAction<boolean>>;
+};
+
+const Add = (props: Props) => {
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    //add user
+    //axios.put(`/api/${slug}s/,{data here})
+  };
+
+  return (
+    <div className="add">
+      <div className="model">
+        <span className="close" onClick={() => props.setOpen(false)}>
+          X
+        </span>
+        <h1>Add new {props.slug}</h1>
+        <form onSubmit={handleSubmit}>
+          {props.columns
+            .filter(
+              (item) =>
+                item.field !== "id" &&
+                item.field != "img" &&
+                item.field != "fullName"
+            )
+            .map((column) => (
+              <div className="item">
+                <label htmlFor="">{column.headerName}</label>
+                <input type={column.type} placeholder={column.field} />
+              </div>
+            ))}
+        </form>
+        <button type="submit">Update</button>
+      </div>
+    </div>
+  );
+};
+
+export default Add;
--- src/pages/users/Users.tsx
+++ src/pages/users/Users.tsx
@@ -2,14 +2,16 @@ import { GridColDef, GridValueGetterParams } from "@mui/x-data-grid";
 import DataTable from "../../components/dataTable/DataTable";
 import "./users.scss";
 import { userRows } from "../../data";
+import { useState } from "react";
+import Add from "../../components/add/Add";
 
 const Users = () => {
   const columns: GridColDef[] = [
-    { field: "id", headerName: "ID", width: 90 },
+    { field: "id", headerName: "ID", width: 40 },
     {
-      field: "avatar",
+      field: "img",
       headerName: "Avatar",
-      width: 100,
+      width: 50,
       renderCell: (params) => {
         return <img src={params.row.img || "/noavatar.png"} alt="No Avatar " />;
       },
@@ -19,35 +21,47 @@ const Users = () => {
       headerName: "First name",
       width: 100,
       editable: true,
+      type: "string",
     },
     {
       field: "lastName",
       headerName: "Last name",
-      width: 120,
+      width: 90,
       editable: true,
+      type: "string",
     },
     {
       field: "email",
       headerName: " Email",
       width: 180,
       editable: true,
+      type: "string",
     },
     {
       field: "phone",
       headerName: " Phone",
-      width: 150,
+      width: 110,
       editable: true,
     },
     {
       field: "age",
       headerName: "Age",
       type: "number",
-      width: 50,
+      width: 40,
       editable: true,
+    },
+    {
+      field: "Yes/No",
+      headerName: "Verified",
+      type: "boolean",
+      width: 80,
+
+      editable: false,
     },
     {
       field: "fullName",
       headerName: "Full name",
+      type: "string",
       description: "This column has a value getter and is not sortable.",
       sortable: false,
       width: 160,
@@ -55,13 +69,15 @@ const Users = () => {
         `${params.row.firstName || ""} ${params.row.lastName || ""}`,
     },
   ];
+  const [open, setOpen] = useState(false);
   return (
     <div className="users">
       <div className="info">
         <h1>Users</h1>
-        <button>Add new Users</button>
+        <button onClick={() => setOpen(true)}>Add new user</button>
       </div>
       <DataTable slug="users" columns={columns} rows={userRows} />
+      {open && <Add slug="user" columns={columns} setOpen={setOpen} />}
     </div>
   );
 };
